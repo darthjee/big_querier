3b77525ff868975dd36f9c1d9a56647398118a9a
darthjee <darthjee@gmail.com>
2016-11-04 00:13:48 -0200

    Connect to mysql

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 (function(module) {
-  var Cloudflare = require('./libs/services/cloudflare'),
-      BigQueryApi = require('./libs/services/big_query/api');
+  var BigQueryApi = require('big_wrapper'),
+      Mysql = require('mysql');
 
   module.exports = {
     handler: function() {
       const projectFile = './config/auth.json',
+            databaseFile = './config/database.json',
             projectData = require(projectFile),
+            databaseConfig = require(databaseFile),
             bigQueryConfig = {
               projectId: projectData['project_id'],
               keyFilename: projectFile
             };
 
       BigQueryApi.default = new BigQueryApi(bigQueryConfig).connect();
-
+      return Mysql.createConnection(databaseConfig);
     }
   };
 })(module);
