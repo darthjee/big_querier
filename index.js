0154e325354080abad8e213f172eda6bf64c63d3
darthjee <darthjee@gmail.com>
2016-11-03 23:51:01 -0200

    Add big query connection

---
+++ index.js
@@ -0,0 +1,18 @@
+(function(module) {
+  var Cloudflare = require('./libs/services/cloudflare'),
+      BigQueryApi = require('./libs/services/big_query/api');
+
+  module.exports = {
+    handler: function() {
+      const projectFile = './config/auth.json',
+            projectData = require(projectFile),
+            bigQueryConfig = {
+              projectId: projectData['project_id'],
+              keyFilename: projectFile
+            };
+
+      BigQueryApi.default = new BigQueryApi(bigQueryConfig).connect();
+
+    }
+  };
+})(module);
